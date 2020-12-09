3a42bc81e2a53f1d990a023f007370f59396b675
myk245 <myk245@nyu.edu>
2020-12-08 21:22:38 -0500

    productArray

---
+++ Algo-Expert/Easy/03-productArray.js
@@ -0,0 +1,14 @@
+// loop through array and for each element, first check whether
+// the element is an array -- if no, just add the element to the /// sum we are keeping track of; if yes, perform a recursive call /// so that we get the sum of the inner array and find how nested /// the array is
+function productSum(array, multiplier = 1) {
+   let sum = 0;
+
+   for (const element of array) {
+      if (Array.isArray(element)) {
+         sum += productSum(element, multiplier + 1);
+      } else {
+         sum += element;
+      }
+   }
+   return sum * multiplier;
+}
