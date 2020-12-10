7b0f9066474799b60f6e0d2359ee5ffc2338014a
myk245 <myk245@nyu.edu>
2020-12-09 23:41:01 -0500

    initial attempt at findThreeLargestNumbers

---
+++ Algo-Expert/Easy/04-findThreeLargestNumbers.js
@@ -0,0 +1,11 @@
+function findThreeLargestNumbers(array) {
+   let largestNums = [];
+
+   for (let i = 0; i < array.length; i++) {
+      for (let j = 1; j < array.length - 1; j++) {
+         if (array[i] > array[j]) {
+            largestNums.push(array[i])
+         } else 
+		}
+   }
+}
