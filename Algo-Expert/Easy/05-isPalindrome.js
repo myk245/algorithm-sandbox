fbb44c0b83d5e771be41b609b5adf76a2afe16e8
myk245 <myk245@nyu.edu>
2020-12-11 17:35:31 -0500

    add notes

--- Algo-Expert/Easy/05-isPalindrome.js
+++ Algo-Expert/Easy/05-isPalindrome.js
@@ -4,8 +4,7 @@ function isPalindrome(string) {
       // if length of string is even
    } else if (string.length % 2 === 0) {
 
-      // if length of string is odd, from the middle letter, check if first half 
-      // of the string reversed equals the second half of the string
+      // if length of string is odd, from the middle letter, check if first half of the string reversed equals the second half of the string
    } else if (string.length % 2 === 1) {
 
    }
