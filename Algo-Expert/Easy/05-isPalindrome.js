2479025541693b28ccfd596d57061f758e4e5b3e
myk245 <myk245@nyu.edu>
2020-12-11 17:34:04 -0500

    begin solving for isPalindrome

---
+++ Algo-Expert/Easy/05-isPalindrome.js
@@ -0,0 +1,12 @@
+function isPalindrome(string) {
+   if (string.length === 1) {
+      return true;
+      // if length of string is even
+   } else if (string.length % 2 === 0) {
+
+      // if length of string is odd, from the middle letter, check if first half 
+      // of the string reversed equals the second half of the string
+   } else if (string.length % 2 === 1) {
+
+   }
+}
