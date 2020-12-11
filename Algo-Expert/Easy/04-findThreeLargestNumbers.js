c5a3074d764d692b966c8d6835075b860a9df32d
myk245 <myk245@nyu.edu>
2020-12-10 19:29:19 -0500

    implement solution for findThreeLargestNumbers

--- Algo-Expert/Easy/04-findThreeLargestNumbers.js
+++ Algo-Expert/Easy/04-findThreeLargestNumbers.js
@@ -1,11 +1,31 @@
-function findThreeLargestNumbers(array) {
-   let largestNums = [];
+// initialize a 3 element long sorted array for the 3 largest numbers traverse through the main array and for each number check if it is larger than the largest number (third element in the threeLargest array) shift and update the threeLargest array accordingly
 
-   for (let i = 0; i < array.length; i++) {
-      for (let j = 1; j < array.length - 1; j++) {
-         if (array[i] > array[j]) {
-            largestNums.push(array[i])
-         } else 
-		}
+function findThreeLargestNumbers(array) {
+   const threeLargest = [null, null, null];
+
+   for (const num of array) {
+      updateLargest(threeLargest, num)
+   }
+   return threeLargest;
+}
+
+function updateLargest(threeLargest, num) {
+   if (threeLargest[2] === null || num > threeLargest[2]) {
+      shiftAndUpdate(threeLargest, num, 2);
+   } else if (threeLargest[1] === null || num > threeLargest[1]) {
+      shiftAndUpdate(threeLargest, num, 1);
+   } else if (threeLargest[0] === null || num > threeLargest[0]) {
+      shiftAndUpdate(threeLargest, num, 0);
+   }
+}
+
+// 
+function shiftAndUpdate(array, num, index) {
+   for (let i = 0; i <= index; i++) {
+      if (i === index) {
+         array[i] = num;
+      } else {
+         array[i] = array[i + 1];
+      }
    }
 }
