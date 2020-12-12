150921b0ed7b508112e2e93e2c438365420fe6b6
myk245 <myk245@nyu.edu>
2020-12-12 15:32:03 -0500

    alternate solution to isPalindrome

--- Algo-Expert/Easy/05-isPalindrome.js
+++ Algo-Expert/Easy/05-isPalindrome.js
@@ -1,11 +1,19 @@
+// function isPalindrome(string) {
+//    if (string.length === 1) {
+//       return true;
+//       // if length of string is even
+//    } else if (string.length % 2 === 0) {
+
+//       // if length of string is odd, from the middle letter, check if first half of the string reversed equals the second half of the string
+//    } else if (string.length % 2 === 1) {
+
+//    }
+// }
+
 function isPalindrome(string) {
-   if (string.length === 1) {
-      return true;
-      // if length of string is even
-   } else if (string.length % 2 === 0) {
-
-      // if length of string is odd, from the middle letter, check if first half of the string reversed equals the second half of the string
-   } else if (string.length % 2 === 1) {
-
+   let reversedString = "";
+   for (let i = string.length - 1; i >= 0; i--) {
+      reversedString += string[i]
    }
+   return string === reversedString;
 }
