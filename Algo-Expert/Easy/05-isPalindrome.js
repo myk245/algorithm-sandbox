8b41fd0069765a3fb4fa2df017bdad4538534b83
myk245 <myk245@nyu.edu>
2020-12-14 21:52:06 -0500

    third alternate solution added

--- Algo-Expert/Easy/05-isPalindrome.js
+++ Algo-Expert/Easy/05-isPalindrome.js
@@ -8,6 +8,20 @@ function isPalindrome(string) {
    return string === reversedChars.join('');
 }
 
+// O(n) time / O(1) space
+function isPalindrome(string) {
+   let leftIndex = 0;
+   let rightIndex = string.length - 1;
+   while (leftIndex < rightIndex) {
+      if (string[leftIndex] !== string[rightIndex]) {
+         return false;
+      }
+      leftIndex++;
+      rightIndex--;
+   }
+   return true;
+}
+
 // O(n^2) time / O(n) space
 function isPalindrome(string) {
    let reversedString = "";
