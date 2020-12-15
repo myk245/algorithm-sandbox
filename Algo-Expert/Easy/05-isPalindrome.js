ad1c9fcfaba358cb8afbaded5030a8e6289a38b3
myk245 <myk245@nyu.edu>
2020-12-14 20:49:10 -0500

    adds space and time complexity

--- Algo-Expert/Easy/05-isPalindrome.js
+++ Algo-Expert/Easy/05-isPalindrome.js
@@ -1,3 +1,12 @@
+// O(n^2) time / O(n) space
+function isPalindrome(string) {
+   let reversedString = "";
+   for (let i = string.length - 1; i >= 0; i--) {
+      reversedString += string[i]
+   }
+   return string === reversedString;
+}
+
 // function isPalindrome(string) {
 //    if (string.length === 1) {
 //       return true;
@@ -9,11 +18,3 @@
 
 //    }
 // }
-
-function isPalindrome(string) {
-   let reversedString = "";
-   for (let i = string.length - 1; i >= 0; i--) {
-      reversedString += string[i]
-   }
-   return string === reversedString;
-}
