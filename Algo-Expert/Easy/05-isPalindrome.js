77497310e954349122a73a97c8924ecdbc36dca9
myk245 <myk245@nyu.edu>
2020-12-14 20:54:07 -0500

    adds alternate solution to isPalindrome

--- Algo-Expert/Easy/05-isPalindrome.js
+++ Algo-Expert/Easy/05-isPalindrome.js
@@ -1,3 +1,13 @@
+// O(n) tiime and space
+function isPalindrome(string) {
+   const reversedChars = [];
+
+   for (let i = string.length - 1; i >= 0; i--) {
+      reversedChars.push(string[i]);
+   }
+   return string === reversedChars.join('');
+}
+
 // O(n^2) time / O(n) space
 function isPalindrome(string) {
    let reversedString = "";
