fcccce7eac8c722a158a8fc9764a7c1d34f9de15
myk245 <myk245@nyu.edu>
2020-12-24 00:53:13 -0500

    cleaned up version of solution to caesarCipher

--- Algo-Expert/Easy/06-caesarCipherEncryptor.js
+++ Algo-Expert/Easy/06-caesarCipherEncryptor.js
@@ -41,3 +41,19 @@ function caesarCipherEncryptor(string, key) {
    }
    return newLetters.join('');
 }
+
+// cleaned up version of above solution
+function caesarCipherEncryptor(string, key) {
+   const newLetters = [];
+   const adjustedKey = key % 26;
+
+   for (const letter of string) {
+      let newLetter = (letter.charCodeAt() + adjustedKey)
+      if (newLetter <= 122) {
+         newLetters.push(String.fromCharCode(newLetter));
+      } else if (newLetter > 122) {
+         newLetters.push(String.fromCharCode(96 + (newLetter % 122)));
+      }
+   }
+   return newLetters.join('')
+}
