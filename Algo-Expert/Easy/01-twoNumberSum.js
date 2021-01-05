937e0c51145752e90f62df1d0e38c258af263745
myk245 <myk245@nyu.edu>
2021-01-04 21:16:11 -0500

    optimize solution for twoNumberSum

--- Algo-Expert/Easy/01-twoNumberSum.js
+++ Algo-Expert/Easy/01-twoNumberSum.js
@@ -3,11 +3,9 @@
 // then check to see if they add up to targetSum
 function twoNumberSum(array, targetSum) {
    for (let i = 0; i < array.length - 1; i++) {
-      const num1 = array[i];
       for (let j = i + 1; j < array.length; j++) {
-         const num2 = array[j];
-         if (num1 + num2 === targetSum) {
-            return [num1, num2];
+         if (array[i] + array[j] === targetSum) {
+            return [array[i], array[j]];
          }
       }
    }
