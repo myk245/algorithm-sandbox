c474c54b8f37ac6388b8896bd502881649691e20
myk245 <myk245@nyu.edu>
2021-01-06 17:51:14 -0500

    add binarySearch

---
+++ 09-binarySearch.js
@@ -0,0 +1,18 @@
+function binarySearch(array, target) {
+   let leftIndex = 0;
+   let rightIndex = array.length - 1;
+
+   while (leftIndex <= rightIndex) {
+      let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+      if (array[middleIndex] === target) {
+         return middleIndex;
+         // if num is larger than target, move right index to left of middleIndex
+      } else if (array[middleIndex] > target) {
+         rightIndex = middleIndex - 1;
+         // if num is lower than target, move left index to the right of middleIndex
+      } else if (array[middleIndex] < target) {
+         leftIndex = middleIndex + 1;
+      }
+   }
+   return -1;
+}
