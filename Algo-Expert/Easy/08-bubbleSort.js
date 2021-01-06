3f87abe206ba8a740785d8ec7e139945af313e27
myk245 <myk245@nyu.edu>
2021-01-05 22:42:34 -0500

    adds notes to bubbleSort

--- Algo-Expert/Easy/08-bubbleSort.js
+++ Algo-Expert/Easy/08-bubbleSort.js
@@ -1,8 +1,10 @@
 function bubbleSort(array) {
    // traverse through array
+   // i is equal to a complete transversal of the array
    for (let i = 0; i < array.length - 1; i++) {
-      // compare number with adjacent number
+     // j represents single iteration of array
       for (let j = 0; j < array.length - 1 - i; j++) {
+         // compare each number with adjacent number
          if (array[j] > array[j + 1]) {
             // if numbers not sorted, swap them
             let temp = array[j];
