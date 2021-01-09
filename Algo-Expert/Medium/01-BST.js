0895534c833ffcf6e1e7636884cbaa6114a9376e
myk245 <myk245@nyu.edu>
2021-01-08 23:57:04 -0500

    delete method for binary search tree

--- Algo-Expert/Medium/01-BST.js
+++ Algo-Expert/Medium/01-BST.js
@@ -29,9 +29,7 @@ class BST {
    }
 
    contains(value) {
-      if (value = this.value) {
-         return true;
-      } else if (value < this.value) {
+      if (value < this.value) {
          if (this.left === null) {
             return false;
          } else {
@@ -44,11 +42,50 @@ class BST {
          } else {
             return this.right.contains(value);
          }
+      } else {
+         return true;
       }
    }
 
-   remove(value) {
-      // Write your code here.
+   remove(value, parent = null) {
+      if (value < this.value) {
+         if (this.left !== null) {
+            this.left.remove(value, this);
+         }
+      } else if (value > this.value) {
+         if (this.right !== null) {
+            this.right.remove(value, this);
+         }
+      } else {
+         if (this.left !== null && this.right !== null) {
+            this.value = this.right.getMinValue();
+            this.right.remove(this.value, this);
+         } else if (parent === null) {
+            if (this.left !== null) {
+               this.value = this.left.value;
+               this.right = this.left.right;
+               this.left = this.left.left;
+            } else if (this.right !== null) {
+               this.value = this.right.value;
+               this.left = this.right.left;
+               this.right = this.right.right;
+            } else {
+               // single node tree, so do nothing
+            }
+         } else if (parent.left === this) {
+            parent.left = this.left !== null ? this.left : this.right;
+         } else if (parent.right === this) {
+            parent.right = this.left !== null ? this.left : this.right;
+         }
+      }
       return this;
+   }
+
+   getMinValue() {
+      if (this.left === null) {
+         return this.value;
+      } else {
+         return this.left.getMinValue();
+      }
    }
 }
