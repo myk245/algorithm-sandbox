b1af5d87de82da232260681f0cf633e9e9913450
myk245 <myk245@nyu.edu>
2021-01-11 17:58:50 -0500

    add solution to adjacentElementsProduct

--- Misc/adjacentElementsProduct.js
+++ Misc/adjacentElementsProduct.js
@@ -1,3 +1,8 @@
 function adjacentElementsProduct(inputArray) {
+   const adjacentProducts = [];
 
+   for (let i = 0; i < inputArray.length - 1; i++) {
+      adjacentProducts.push((inputArray[i]) * (inputArray[i + 1]))
+   }
+   return Math.max(...adjacentProducts);
 }
