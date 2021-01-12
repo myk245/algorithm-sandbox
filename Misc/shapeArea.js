cc2373616c9539cf399acd06e5c3a3217b1bfce8
myk245 <myk245@nyu.edu>
2021-01-12 16:43:25 -0500

    alternate solution to shapeArea

--- Misc/shapeArea.js
+++ Misc/shapeArea.js
@@ -1,4 +1,12 @@
 function shapeArea(n) {
+   if (n === 1) {
+      return 1;
+   } else {
+      return shapeArea(n - 1) + (4 * (n - 1));
+   }
+}
+
+function shapeArea(n) {
    let area = 1;
 
    for (let i = 1; i < n; i++) {
