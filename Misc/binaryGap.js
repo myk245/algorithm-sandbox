7e388c41c757ce76e71224ae8eb0ba1f79737340
myk245 <myk245@nyu.edu>
2021-02-04 23:13:30 -0500

    set up binary gap

---
+++ Misc/binaryGap.js
@@ -0,0 +1,4 @@
+// write a function that given a positive integer, returns the length of its longest binary gap - return 0 if integer doesn't contain a binary gap
+function binaryGap(integer) {
+
+}
--- Misc/missingInteger.js
+++ Misc/missingInteger.js
@@ -1,7 +1,7 @@
 // given an array of integers, return the smallest positive integer not in the array
 
 // initial solution -- not the most efficient
-// O(N**2) time
+// O(N^2) time
 function solution(A) {
    // smallest positive integer not in the array has to be between the smallest and the biggest number that does appear in the array
    let sortedArray = A.sort();
@@ -12,4 +12,19 @@ function solution(A) {
       smallestPosInt++;
    }
    return smallestPosInt;
+}
+
+// since we only consider unique values, make use of the set data structure
+// O(N) time 
+function solution(A) {
+   let setFromArray = new Set(A);
+   let i = 1;
+
+   for (i; i <= setFromArray.size; i++) {
+      if (setFromArray.has(i)) {
+         continue;
+      }
+      break;
+   }
+   return i;
 }
