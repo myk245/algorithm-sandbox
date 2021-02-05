1ef5a92ab6aec1ca7607f053d2ef459485aa9466
myk245 <myk245@nyu.edu>
2021-02-04 23:57:26 -0500

    general outline of solution

--- Misc/binaryGap.js
+++ Misc/binaryGap.js
@@ -1,4 +1,17 @@
 // write a function that given a positive integer, returns the length of its longest binary gap - return 0 if integer doesn't contain a binary gap
-function binaryGap(integer) {
 
+// function binaryGap(integer) {
+//    // number.toString(2) outputs binary representation of number
+//    console.log(integer.toString(2))
+// }
+
+function solution(N) {
+   // console.log(N.toString(2))
+   // turn number into binary representation with .toString(2) method -> returns a string
+   let binaryRep = N.toString(2);
+   // set up 2 pointers
+   let leftPointer = 0;
+   let rightPointer = 0;
+   // iterate through binaryRep starting at index 0
 }
+
