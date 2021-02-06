f21d81ec670136fc081e3e1f5314c1641b5b4762
myk245 <myk245@nyu.edu>
2021-02-06 18:40:36 -0500

    add note

--- Misc/cyclicRotation.js
+++ Misc/cyclicRotation.js
@@ -1,6 +1,6 @@
 // write a function that given an array A consisting of N integers and an integer K, returns the array A rotated K times
 function solution(A, K) {
-   K = K % A.length;
+   K = K % A.length; // to handle large K's
    let elementsToMoveToFront = A.slice(A.length - K);
    let elementsToMoveToBack = A.slice(0, A.length - K);
    return [...elementsToMoveToFront, ...elementsToMoveToBack];
