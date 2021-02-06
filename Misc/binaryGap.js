d707b95c1eb456f194c4f3a67428f714bd4bbd7e
myk245 <myk245@nyu.edu>
2021-02-06 00:43:32 -0500

    full solution to binary gap

--- Misc/binaryGap.js
+++ Misc/binaryGap.js
@@ -4,36 +4,20 @@ function solution(N) {
    let binaryRep = N.toString(2) // returns a string 
    let currentGap = 0;
    let biggestGap = 0;
-   // loop through the binaryRep starting from the right-most digit
+
    for (let i = 0; i < binaryRep.length; i++) {
-      // the start of a binary gap has to be a '0'
+      // the start of a binary gap has to be a '0', so we only increase current binary gap once we come upon our first '0'
       if (binaryRep[i] === '0') {
          currentGap++;
-         // if we come upon a '1', check if if it is 
+         // if we come upon a '1', this represents the end of a gap, so compare current gap against biggest gap
       } else if (binaryRep[i] === '1') {
          if (currentGap > biggestGap) {
             biggestGap = currentGap;
          }
+         // reset current gap
          currentGap = 0;
       }
    }
    return biggestGap;
-}
-
-function solution(N) {
-   let binaryRep = N.toString(2).split(''); // each element of array is a string
-   let oneCount = 0;
-   // there is only a binary gap if there is more than 1 '1' 
-   for (let i = 0; i < binaryRep.length; i++) {
-      if (binaryRep[i] === '1') {
-         oneCount++;
-      }
-   }
-   // check if oneCount is more than 1 -- if yes, proceed to find longest binary gap
-   if (oneCount > 1) {
-
-   } else {
-      return 0;
-   }
 }
 
