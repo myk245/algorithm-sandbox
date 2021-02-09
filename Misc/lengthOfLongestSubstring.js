4c6ef3a3c3b4bbbbce5748d17b700054b437fe9c
myk245 <myk245@nyu.edu>
2021-02-08 20:36:31 -0500

    add notes

--- Misc/lengthOfLongestSubstring.js
+++ Misc/lengthOfLongestSubstring.js
@@ -1,11 +1,15 @@
+// sliding window approach
+// O(N) time
 var lengthOfLongestSubstring = function (s) {
-   let set = new Set(); 
+   let set = new Set(); // store only unique values
    let longestSubstringLength = 0; 
    let firstPointer = 0;
 
    for (let secondPointer = 0; secondPointer < s.length; secondPointer++) {
+      // handle repeating values -- delete the left-most occurrence from set
       while (set.has(s[secondPointer])) {
          set.delete(s[firstPointer]);
+         // move first pointer over one space to the right
          firstPointer++; 
       }
       set.add(s[secondPointer]);
