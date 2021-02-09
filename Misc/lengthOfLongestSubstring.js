3c6bfe29da646d69bc3eac9fc404f9637851e63d
myk245 <myk245@nyu.edu>
2021-02-08 19:28:21 -0500

    working solution

--- Misc/lengthOfLongestSubstring.js
+++ Misc/lengthOfLongestSubstring.js
@@ -1,23 +1,15 @@
 var lengthOfLongestSubstring = function (s) {
-   let longestSubstringLength = 0;
-   let currentSubstringLength = 1; // first character will always be a unique substring
-   // loop through array with 2 pointers
-   // first pointer loops through array
-   // second pointer checks for a repeating character
-   for (let i = 0; i < s.length; i++) {
-      for (let j = 1; j < s.length - 1; j++) {
-         // increase currentSubstringLength by 1 every time we come upon a unique character
-         if (s[i] !== s[j]) {
-            currentSubstringLength++;
-            // if the letters are the same, then this is the end of the substring without repeating characters
-         } else {
-            // compare currentSubstringLength to longestSubstringLength
-            if (currentSubstringLength > longestSubstringLength) {
-               longestSubstringLength = currentSubstringLength;
-            }
-            currentSubstringLength = 0;
-         }
+   let set = new Set(); 
+   let longestSubstringLength = 0; 
+   let firstPointer = 0;
+
+   for (let secondPointer = 0; secondPointer < s.length; secondPointer++) {
+      while (set.has(s[secondPointer])) {
+         set.delete(s[firstPointer]);
+         firstPointer++; 
       }
+      set.add(s[secondPointer]);
+      longestSubstringLength = Math.max(longestSubstringLength, secondPointer - firstPointer + 1);
    }
    return longestSubstringLength;
 };
