ffad5e0a46cf99724d5906d1602e54a33d278996
myk245 <myk245@nyu.edu>
2021-02-08 20:57:46 -0500

    finished solution

--- Misc/lengthOfLongestSubstring.js
+++ Misc/lengthOfLongestSubstring.js
@@ -9,11 +9,36 @@ var lengthOfLongestSubstring = function (s) {
       // handle repeating values -- delete the left-most occurrence from set
       while (set.has(s[secondPointer])) {
          set.delete(s[firstPointer]);
-         // move first pointer over one space to the right
+         // only move first pointer over to the right when we come upon a repeating value 
          firstPointer++; 
       }
       set.add(s[secondPointer]);
+      // update longesrSubstringLength by comparing old value of longestSubstringLength to the length of the current substring
       longestSubstringLength = Math.max(longestSubstringLength, secondPointer - firstPointer + 1);
    }
    return longestSubstringLength;
 };
+
+function lengthOfLongestSubstring(string) {
+   let pointerA = 0; 
+   let pointerB = 0; 
+   let maxLength = 0; 
+   let substring = new Set(); 
+
+   while (pointerB < string.length) {
+      // if our Set doesn't already contain value at pointerB, add it to the set
+      if (!substring.has(string[pointerB])) {
+         substring.add(string[pointerB]);
+         // increment pointerB -- make window bigger
+         pointerB++; 
+         // check to see if we need to update maxLength
+         maxLength = Math.max(maxLength, substring.size); 
+         // if the value at pointerB exists in our Set already, delete the character from pointerA in the string from the set
+      } else {
+         substring.delete(string[pointerA]);
+         // increment pointerA -- moving window forward
+         pointerA++; 
+      }
+   }
+   return maxLength; 
+}
