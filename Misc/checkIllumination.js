22274a0f7cdb849f0eef6eceb23855ed67c56af4
myk245 <myk245@nyu.edu>
2021-02-10 22:29:55 -0500

    number of valid words

--- Misc/checkIllumination.js
+++ Misc/checkIllumination.js
@@ -3,6 +3,7 @@
 // M = list of M entrues; each entry is a list of 2 entries representing the x, y coordinates of a query cell
 // output should be a list of M entries; each entry will either be "LIGHT" or "DARK"
 
+// very difficult - will need to come back to this
 function checkIllumination(N, lamps, queries) {
    // keep track of illuminated cells: row, column, and both diagonals
    let rowCells = {};
---
+++ Misc/numberOfValidWords.js
@@ -0,0 +1,7 @@
+// given a puzzle string, a word is valid if:
+   // 1. word contains the first letter of puzzle
+   // 2. for each letter in word, that letter is in puzzle
+   // Return an array answer, where answer[i] is the number of words in the given word list words that are valid with respect to the puzzle puzzles[i].
+var findNumOfValidWords = function (words, puzzles) {
+
+};
