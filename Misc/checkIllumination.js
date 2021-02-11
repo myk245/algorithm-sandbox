739eb843af90caff15e7add1425a0641ec4f8c0b
myk245 <myk245@nyu.edu>
2021-02-10 22:16:47 -0500

    begin thinking of how to implement solution

---
+++ Misc/checkIllumination.js
@@ -0,0 +1,12 @@
+// N = integer representing size of grid
+// lamps = list of L entries; each entry is a list with 2 entries representing the x, y coordinates of a lamo
+// M = list of M entrues; each entry is a list of 2 entries representing the x, y coordinates of a query cell
+// output should be a list of M entries; each entry will either be "LIGHT" or "DARK"
+
+function checkIllumination(N, lamps, queries) {
+   // keep track of illuminated cells: row, column, and both diagonals
+   let rowCells = {};
+   let columnCells = {};
+   let forwardDiagonalCells = {};
+   let backwardDiagonalCells = {};
+}
