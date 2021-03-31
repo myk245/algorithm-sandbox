b188cae9e504fe678ec8e56aee980a8a58c46b40
myk245 <myk245@nyu.edu>
2021-03-30 23:43:51 -0400

    memoization approach

--- Misc/minStepsToOne.js
+++ Misc/minStepsToOne.js
@@ -34,4 +34,48 @@ function minStepsToOne(n) {
    return res; 
 }
 
+// memoization
+function minStepsToOne(n) {
+   let cache = {};
+   
+   function recurse(curr) {
+      // base case
+      if (curr === 1) {
+         return 0;
+      }
+
+      // check cache 
+      if (curr in cache) {
+         return cache[curr];
+      }
+
+      // recursive relations
+      // subtract 1 
+      let steps = recurse(curr - 1)
+
+      // divide by 2
+      if (curr % 2 === 0) {
+         let divby2 = recurse(curr / 2);
+         steps = Math.min(steps, divby2)
+      }
+
+      // divide by 3
+      if (curr % 3 === 0) {
+         let divby3 = recurse(curr / 3);
+         steps = Math.min(steps, divby3)
+      }
+      // return min # of steps to 1 from curr
+      let result = 1 + steps;
+      // store result in cache 
+      cache[curr] = result;
+      
+      return result;
+   }
+
+   let res = recurse(n);
+   return res;
+}
+
+console.time("Brute Force Recursion: "); 
 console.log(minStepsToOne(10));
+console.timeEnd("Brute Force Recursion: ")
