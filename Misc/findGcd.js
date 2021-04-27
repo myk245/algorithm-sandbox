21985a33ce748b53b9357f1d553fc360f262e0a4
myk245 <myk245@nyu.edu>
2021-04-27 16:29:36 -0400

    refactor using destructuring assignment

--- Misc/findGcd.js
+++ Misc/findGcd.js
@@ -27,3 +27,28 @@ function findGcd(a, b) {
    // return the non zero value
    return a; 
 }
+
+// refactor using destructuring assigment
+function findGcd(a, b) {
+   // if inputs are negative, convert to positive
+   if (a < 0) {
+      a *= -1;
+   }
+   if (b < 0) {
+      b *= -1;
+   }
+
+   // swap inputs if a < b using destructuring assignment
+   if (a < b) {
+      [a, b] = [b, a]; 
+   }
+
+   // use modulo to reduce the larger value until one number is zero
+   while (b > 0) {
+      // set a to b and b to the modulo of a and b
+      [a, b] = [b, a % b]; 
+   }
+
+   // return the non zero value
+   return a; 
+}
